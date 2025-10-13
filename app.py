4bff6e26d5d95dc7c5e8ebad56c3bc8f28d05ae0
Denys <146959885+fxtortic@users.noreply.github.com>
2025-10-13 22:04:39 +0300

    feat(lab2): create resume and all form tasks

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return 'Hello, world!'
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', page_title='Моє резюме')
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title='Контакти')
+
+@app.route("/form")
+def form():
+    return render_template("form.html", page_title='Форма')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
